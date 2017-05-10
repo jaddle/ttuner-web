a48815a89a213b73a2c1fbeeba1f57f643209e70
Jonathan Addleman <jono@redowl.ca>
2017-05-09 21:00:34 -0400

    added 1/6 comma ordinaire

--- temperaments.js
+++ temperaments.js
@@ -33,4 +33,21 @@ var availableTemperaments =
 	{"note1": "B♭", "note2": "E♭", "fraction": "-1/6", "comma": "S"},
 	{"note1": "E♭", "note2": "A♭", "fraction": "-1/6", "comma": "S"},
 	{"note1": "A♭", "note2": "D♭", "fraction": "-1/6", "comma": "S"}
-]}];
+]},
+
+{"name": "1/6 Comma ordinaire", "description": "1/6 comma meantone between F and F#, pure fifths around the sharps, and Bb-F. Ends up like Valotti, but F#-D# are all a bit lower, so it favours sharps, and is a little crunchier on the extreme end, C#, F# major chords.", "rules": [
+	{"note1": "A", "note2": "E", "fraction": "-1/6", "comma": "S"},
+	{"note1": "E", "note2": "B", "fraction": "-1/6", "comma": "S"},
+	{"note1": "B", "note2": "F#", "fraction": "-1/6", "comma": "S"},
+	{"note1": "F#", "note2": "C#", "fraction": "-1/6", "comma": "S"},
+	{"note1": "C#", "note2": "G#", "fraction": "", "comma": "S"},
+	{"note1": "G#", "note2": "D#", "fraction": "", "comma": "S"},
+	{"note1": "A", "note2": "D", "fraction": "-1/6", "comma": "S"},
+	{"note1": "D", "note2": "G", "fraction": "-1/6", "comma": "S"},
+	{"note1": "G", "note2": "C", "fraction": "-1/6", "comma": "S"},
+	{"note1": "C", "note2": "F", "fraction": "-1/6", "comma": "S"},
+	{"note1": "F", "note2": "B♭", "fraction": "", "comma": "S"},
+]}
+
+];
+
